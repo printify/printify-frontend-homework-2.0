d69ad73fb423d756f4364155f16f590b27636591
Iryna Antonets <i.antonets@spheremall.com>
2021-02-05 16:09:48 +0200

    Iryna Mishcheriakova interview

--- 3-fizz-buzz/src/index.ts
+++ 3-fizz-buzz/src/index.ts
@@ -1,7 +1,16 @@
 export function fizzBuzz(): (string | number)[] {
   const resultArray: (string | number)[] = [];
-
-  // ...
+  for(let i = 1; i <=100; i++) {
+    let result: string | number = i;
+    if(!(i%3) && !(i%5)) {
+      result = 'FizzBuzz';
+    } else if(!(i%3)) {
+      result = 'Fizz';
+    } else if(!(i%5)) {
+      result = 'Buzz'; 
+    }
+    resultArray.push(result);
+  }
 
   return resultArray;
 }
