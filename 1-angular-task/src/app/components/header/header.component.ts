ea594d80398c6fc30852c73d9490d165a611953d
Rolands Jegorovs <rolandjegorov@gmail.com>
2024-03-04 21:33:09 +0200

    Update to angular 17, minor refactoring

--- 1-angular-task/src/app/components/header/header.component.ts
+++ 1-angular-task/src/app/components/header/header.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HeaderComponent {}
