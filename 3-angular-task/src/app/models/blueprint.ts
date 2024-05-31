aa7bf761728fed850126663774607471e2906fff
Janis Zarzeckis <janis.zarzeckis@printify.com>
2024-05-31 11:54:20 +0300

    Replace the lost mockly endpoint with a new one

--- 3-angular-task/src/app/models/blueprint.ts
+++ 3-angular-task/src/app/models/blueprint.ts
@@ -1,9 +1,39 @@
-export interface Blueprint {
-  id: number;
-  model: string;
+interface BlueprintImage {
+  catalogPreview: 'Hover Feature'[];
+  /** To get the URL this src needs to be added in this URL as: `https://printify.com/cdn-cgi/image/width=320,quality=100,format=avif/https://images.printify.com/api/catalog/${src}.jpg` */
+  src: string;
+}
+
+interface Blueprint {
+  blueprintId: number;
+  created_at: string;
+  tags: string[];
+  images: BlueprintImage[];
   name: string;
-  price: number;
-  brand: string;
-  printProvider: string;
-  imageUrl: string;
+  printProviderName: string;
+  brandname: string;
+  targetMarkets: string[];
+}
+
+/** The API endpoint response type for blueprint list */
+export type Blueprints = Blueprint[];
+
+/** The API endpoint resonse type when requesting a single blueprint */
+export interface SingleBlueprint {
+  blueprintId: number;
+  description: string;
+  details: string[];
+  features: { name: string; description: string }[];
+  media: (
+    | {
+        /** ID that needs to be placed in an url as follows: `https://printify.com/cdn-cgi/image/width=520,quality=100,format=avif/https://images.printify.com/api/catalog/${src}` */
+        src: string;
+      }
+    | {
+        /** HTML element that should be mounted */
+        video: string;
+      }
+  )[];
+  name: string;
+  tags: string;
 }
